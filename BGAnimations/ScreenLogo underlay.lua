dabb31d38391ec79974acb88a6b2a9d010d1b2d7
Kysee Mata <akmata09@gmail.com>
2018-11-21 21:10:38 -0600

    logo animation so far

--- BGAnimations/ScreenLogo underlay.lua
+++ BGAnimations/ScreenLogo underlay.lua
@@ -33,24 +33,27 @@ local t = Def.ActorFrame{
 }
 
 -- Draw DDRIllini letters
-for i=1,9 do
+-- for i=1,9 do
 	t[#t+1] = Def.ActorFrame {
-		LoadActor("ScreenTitleMenu underlay/ddrillini/" .. ddrillini_letters[i] .. ".png") .. {
+		-- LoadActor("ScreenTitleMenu underlay/ddrillini/" .. ddrillini_letters[i] .. ".png") .. {
+		LoadActor("ddrillini_logo_cut_up.mp4") .. {
 	-- There appear to be two different syntaxes: `function(self)` and `cmd`.
 			InitCommand=function(self)
 				self
 				-- positioning, sizing, shadow
 				-- added :Center and :y(2) relative to the other instance of 
 				-- this code in ScreenTitleMenu
-				:Center() :y(2) :zoom(0.7) :shadowlength(0.75)
+				:Center() :y(2) :zoom(0.3) :shadowlength(0.75)
 				-- fading in
 				:diffusealpha(0)
+				:blend("add")
+				:GetTexture("ddrillini.png")
 			end,
 
 			OnCommand=function(self)
 				self
 				-- delay each letter by a bit more each time
-				:sleep(i*0.1 + 0.2)
+				-- :sleep(i*0.1 + 0.2)
 				:linear(.75)
 				:diffusealpha(1)
 			end,
@@ -62,9 +65,10 @@ for i=1,9 do
 				:shadowlength(0)
 			end,
 
+
 		}
 	}
-end
+-- end
 
 -- Check if coins have been inserted. Display corresponding elements appropriately.
 local af = Def.ActorFrame{
